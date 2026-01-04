f23099f4b4cfe6821824184b20231ea9649da8e2
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-04 16:50:57 +0530

    update

--- src/middleware/role.ts
+++ src/middleware/role.ts
@@ -4,15 +4,19 @@ import { AuthRequest } from "./auth";
 
 export const requireRole = (roles: Role[]) =>{
     return (req: AuthRequest, res: Response, next: NextFunction) =>{
+console.log("role.ts", req)
         if(!req.user){
             return res.status(401).json({message:"Unauthorized"})
         }
+        
     
-
-        const hasRole = roles.some((role) => req.user.roles?.includes(role))
+console.log("role.tssss")
+        const hasRole = roles.some((role) => req.user.role?.includes(role))
+        console.log(hasRole)
         if (!hasRole){
-            return res.status(403).json({message: `Require ${roles} role`})
+            return res.status(405).json({message: `Require ${roles} role`})
         }
+console.log("role.ts done")
         next()
     }
 }
