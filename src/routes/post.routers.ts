06b54d840782921b76d2416a2ba9ba1a7b46d03c
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-04 16:50:35 +0530

    update routes

--- src/routes/post.routers.ts
+++ src/routes/post.routers.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import { authenticate } from "../middleware/auth";
 import { requireRole } from "../middleware/role";
 import { Role } from "../models/User";
-import { saveRoom } from "../controllers/rooms.controller";
-import { savePackage } from "../controllers/pacage.controller";
+import { deleteRoom, getAllRooms, saveRoom } from "../controllers/rooms.controller";
+import {  getAllPackages, savePackage } from "../controllers/pacage.controller";
+import { upload } from "../middleware/upload";
 
 const route = Router()
 
@@ -11,13 +12,34 @@ route.post(
     "/createRoom",
     authenticate,
     requireRole([Role.ADMIN, Role.AUTHOR]),
+    upload.single("image"),
     saveRoom
 )
+
+route.get(
+    "/getAll",
+    getAllRooms
+)
+
+route.post(
+  "/roomDelete",
+  authenticate,
+  requireRole([Role.ADMIN, Role.AUTHOR]),
+  deleteRoom
+)
+
 
 route.post(
     "/createPackage",
     authenticate,
     requireRole([Role.ADMIN, Role.AUTHOR]),
+    upload.single("image"),
     savePackage
 )
+
+route.get(
+    "/getAllPackage",
+    getAllPackages
+)
+
 export default route
